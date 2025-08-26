d6bf099673828f0fbaf9ae8a9c62151b350bd4e0
Sergey3G <serhiosmol@gmail.com>
2025-08-26 19:16:07 +0300

    twenyth commit

--- square_equations.cpp
+++ square_equations.cpp
@@ -7,17 +7,28 @@
 #include "Solver.hpp"
 #include "Input.hpp"
 #include "Printer.hpp"
-#include "Tester.hpp"
+#include "Tester.hpp" // TODO: __LINE__ __FUNCTION__ __FILE__
 
 
-int main(void) // TODO: argc argv "--test"
+int main(const int argc, char* const argv[])
 {
-    double a = NAN, b = NAN, c = NAN;
-    InputProcessing(&a, &b, &c);
-    double x1 = NAN, x2 = NAN;
-    enum NRoots nRoots = SolveSquare(a, b, c, &x1, &x2);
-    PrintRoots(nRoots, x1, x2);
-    TestManySolveSquare();
+    if (argc > 1)
+    {
+        printf("argc = %d\nargv[1] = %s\n", argc, argv[1]);
+        if (strncmp(argv[1], "--solve", 7) == 0)
+        {
+            double a = NAN, b = NAN, c = NAN;
+            InputProcessing(&a, &b, &c);
+            double x1 = NAN, x2 = NAN;
+            enum NRoots nRoots = SolveSquare(a, b, c, &x1, &x2);
+            PrintRoots(nRoots, x1, x2);
+        }
+
+        if (strncmp(argv[1], "--test", 6) == 0)
+        {
+            TestManySolveSquare();
+        }
+    }
     return 0;
 }
 
