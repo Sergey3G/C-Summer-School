2036336d10e124d1f7b8c6521ba43f4ea0f72d78
Sergey3G <serhiosmol@gmail.com>
2025-08-28 22:31:37 +0300

    twenty_nineth_commit

--- include/Asserts.hpp
+++ include/Asserts.hpp
@@ -1,11 +1,15 @@
 #ifndef ASSERTS_H
 #define ASSERTS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 #define MY_ASSERT(condition, message) \
     if (!(condition)) \
     { \
         fprintf(stderr, "Assertion failed: %s, file: %s, function: %s, line: %d.\n", \
-                message, __FILE__, __FUNCTION__, __LINE__);\
+                message, __FILE__, __FUNCTION__, __LINE__); \
+        abort(); \
     }
 
 #endif
